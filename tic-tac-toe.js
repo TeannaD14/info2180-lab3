c05e2b4f8a9ae8a35ab3e7d1b513bfb95edad4e5
Teanna Dixon <teannadixon09@gmail.com>
2025-10-17 20:40:34 -0500

    Excercise3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -24,3 +24,36 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+window.addEventListener('DOMContentLoaded', () => {
+  const squ = document.querySelectorAll('#board div');
+  const stat = document.getElementById('status');
+  let activeplayer = 'X';
+  let statonboard = Array(9).fill(null);
+
+  squ.forEach((square, index) => {
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => {
+      square.classList.add('hover');
+    });
+
+    square.addEventListener('mouseout', () => {
+      square.classList.remove('hover');
+    });
+
+    square.addEventListener('click', () => {
+      if (!statonboard[index]) {
+        square.textContent = activeplayer;
+        square.classList.add(activeplayer);
+        statonboard[index] = activeplayer;
+
+        check();
+
+        activeplayer = activeplayer === 'X' ? 'O' : 'X';
+      }
+    });
+  });
+
+  function check() {
+  }
+});
